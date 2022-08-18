09c21431d436d06218c4b1ff8b90bcc14079142d
Anita Lu <r10921058@ntu.edu.tw>
2022-08-18 22:12:33 +0800

    :sparkles: fixed #3

--- src/components/HostProfile/index copy.js
+++ src/components/HostProfile/index copy.js
@@ -8,39 +8,38 @@
 // *
 // * ////////////////////////////////////////////////////////////////////////
 
-import "./index.scss";
-import Loader from "react-loaders";
-import configData from "../../config.json";
-import AnimatedLetters from "../AnimatedLetters";
-import React, { useState, useEffect } from "react";
-import { useSpring, animated, config } from "react-spring";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSquareEnvelope } from "@fortawesome/free-solid-svg-icons";
+import './index.scss'
+import Loader from 'react-loaders'
+import configData from '../../config.json'
+import AnimatedLetters from '../AnimatedLetters'
+import React, { useState, useEffect } from 'react'
+import { useSpring, animated, config } from 'react-spring'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSquareEnvelope } from '@fortawesome/free-solid-svg-icons'
 import {
   faLinkedin,
   faSquareFacebook,
   faSquareGithub,
-  faLine,
-} from "@fortawesome/free-brands-svg-icons";
+} from '@fortawesome/free-brands-svg-icons'
 
 const HostProfile = () => {
   // * HostProfile: Implement "Host Profile" page.
   // @param props:          useSpring   the animation for animated.div
   //        letterClass     String      the animation for title
-
-  const [letterClass, setLetterClass] = useState("text-animate");
+  
+  const [letterClass, setLetterClass] = useState('text-animate')
   const props = useSpring({
     to: { opacity: 1 },
     from: { opacity: 0 },
     reset: false,
     delay: 2000,
     config: config.molasses,
-  });
+  })
 
   // * Scroll to top of the page when rendering
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+    window.scrollTo(0, 0)
+  }, [])
 
   return (
     <>
@@ -50,41 +49,33 @@ const HostProfile = () => {
             <h1>
               <AnimatedLetters
                 letterClass={letterClass}
-                strArray={configData.HOST_PROFILE_PAGE.NAME.split("")}
-                idx={4}
+                strArray={configData.HOST_PROFILE_PAGE.NAME.split('')}
+                idx={5}
               />
             </h1>
           </div>
           <animated.div className="content" style={props}>
-            <div className="header">
-              <p>"{configData.HOST_PROFILE_PAGE.HEADER}"</p>
-            </div>
             <div className="content-info">
               <div className="left-col">
-                {configData.HOST_PROFILE_PAGE.SHORT_BIO.map((paragraph) => (
-                  <p>{paragraph}</p>
-                ))}
-              </div>
-              <div className="right-col">
-                <div>
                 <div className="img-container">
                   <img src={configData.HOST_PROFILE_PAGE.PHOTO} />
                 </div>
+              </div>
+              <div className="right-col">
+                <ul className="position-list">
+                  {configData.HOST_PROFILE_PAGE.POSITION.map((item) => (
+                    <li>{item}</li>
+                  ))}
+                </ul>
                 <div className="media-list">
                   <a
-                    href={"mailto:" + configData.HOST_PROFILE_PAGE.INFO.EMAIL}
+                    href={'mailto:' + configData.HOST_PROFILE_PAGE.INFO.EMAIL}
                     target="_blank"
                   >
                     <FontAwesomeIcon
                       className="media-icon"
                       icon={faSquareEnvelope}
                     />
-                  </a>
-                  <a
-                    href={configData.HOST_PROFILE_PAGE.INFO.LINKEDIN}
-                    target="_blank"
-                  >
-                    <FontAwesomeIcon className="media-icon" icon={faLinkedin} />
                   </a>
                   <a
                     href={configData.HOST_PROFILE_PAGE.INFO.FACEBOOK}
@@ -96,26 +87,31 @@ const HostProfile = () => {
                     />
                   </a>
                   <a
-                    href={configData.HOST_PROFILE_PAGE.INFO.LINE}
+                    href={configData.HOST_PROFILE_PAGE.INFO.GITHUB}
                     target="_blank"
                   >
-                    <FontAwesomeIcon className="media-icon" icon={faLine} />
+                    <FontAwesomeIcon
+                      className="media-icon"
+                      icon={faSquareGithub}
+                    />
+                  </a>
+                  <a
+                    href={configData.HOST_PROFILE_PAGE.INFO.LINKEDIN}
+                    target="_blank"
+                  >
+                    <FontAwesomeIcon className="media-icon" icon={faLinkedin} />
                   </a>
                 </div>
-
-                </div>
-                
               </div>
             </div>
-
-            {/* <div className="education">
+            <div className="education">
               <h3>Education</h3>
               <ul className="edu-list">
                 <li>B.S. {configData.HOST_PROFILE_PAGE.EDUCATION.BACHELOR}</li>
                 <li>Ph.D. {configData.HOST_PROFILE_PAGE.EDUCATION.PHD}</li>
               </ul>
-            </div> */}
-            {/* <div className="education">
+            </div>
+            <div className="education">
               <h3>{configData.HOST_PROFILE_PAGE.MAJOR_RESEARCH_AREA.TITLE}</h3>
               <ul className="edu-list">
                 {configData.HOST_PROFILE_PAGE.MAJOR_RESEARCH_AREA.CONTENTS.map(
@@ -127,8 +123,8 @@ const HostProfile = () => {
                   )
                 )}
               </ul>
-            </div> */}
-            {/* <div className="education">
+            </div>
+            <div className="education">
               <h3>{configData.HOST_PROFILE_PAGE.RESEARCH_SUMMARY.TITLE}</h3>
               <ul className="edu-list">
                 {configData.HOST_PROFILE_PAGE.RESEARCH_SUMMARY.CONTENTS.map(
@@ -137,13 +133,13 @@ const HostProfile = () => {
                   )
                 )}
               </ul>
-            </div> */}
+            </div>
           </animated.div>
         </div>
       </div>
       <Loader type="line-scale" />
     </>
-  );
-};
+  )
+}
 
-export default HostProfile;
+export default HostProfile
